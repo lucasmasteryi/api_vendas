aad60bc4d0c5974cae533d20528d86df2b16b5be
lucasmasteryi <lucasmatheus.adm98@gmail.com>
2023-12-02 12:10:10 -0300

    End Point de Delete

--- src/main/java/com/vendas/api/controllers/ProductController.java
+++ src/main/java/com/vendas/api/controllers/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController()
 @RequestMapping("product")
@@ -30,5 +31,15 @@ public class ProductController {
     public ResponseEntity getAllProducts(){
         List<ProductResponseDTO> productList = this.productRepository.findAll().stream().map(ProductResponseDTO::new).toList();
         return ResponseEntity.ok(productList);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> remove(@PathVariable String id) {
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isPresent()) {
+            productRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
